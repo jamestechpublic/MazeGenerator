7417c6f67b46de96380969dc951ecd55597aaf64
James Lombard <jtrlombard@gmail.com>
2024-04-18 10:22:21 +0200

    Add project files.

---
+++ MazeGenerator/AppLogic/MazeGen.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace MazeGenerator.AppLogic
+{
+    [Flags]
+    internal enum WallState
+    {
+        LEFT = 1, // 0001
+        RIGHT = 2, // 0010
+        UP = 4, // 0100
+        DOWN = 8, // 1000
+
+        VISITED = 128 // 1000 0000
+    }
+    // WallState wallstate = WallState.LEFT | WallState.Right;    0011
+    // wallstate |= WallState.DOWN;  0111 adds a state
+    // wakkstate &= ~WallState.RIGHT; 0101 removes a state by using inverse of right
+
+    internal struct Position
+    {
+        public int X;
+        public int Y;
+    }
+
+    internal struct Neighbour
+    {
+        public Position Position;
+        public WallState SharedWall;
+    }
+
+    internal class MazeGen
+    {
+        public WallState[,] Generate(int width, int height)
+        {
+            WallState[,] maze = new WallState[width, height];
+            
+            var allWalls = WallState.LEFT | WallState.RIGHT | WallState.DOWN | WallState.UP;
+            for (int i = 0; i < width; i++)
+                for (int j = 0; j < height; j++)
+                    maze[i, j] = allWalls; // 1111
+
+            // maze[i,j].HasFlag(WallState.Right);
+
+            return ApplyRecursiveBackTracker(maze, width, height);
+        }
+
+        private WallState[,] ApplyRecursiveBackTracker(WallState[,] maze, int width, int height)
+        {
+            var rnd = new System.Random(/* seed */);
+            var posStack = new Stack<Position>();
+            var pos = new Position { X = rnd.Next(0,width), Y = rnd.Next(0,height) };
+            
+            maze[pos.X, pos.Y] |= WallState.VISITED; // 1000 1111
+            posStack.Push(pos);
+
+            while (posStack.Count > 0)
+            {
+                var current = posStack.Pop();
+                var neighbours = GetUnvisitedNeighbours(pos, maze, width, height);
+
+                if (neighbours.Count > 0)
+                {
+                    posStack.Push(current); // put back on stack
+
+                    var rndIndex = rnd.Next(0, neighbours.Count); // random unvisited index
+                    var randomNeighbour = neighbours[rndIndex]; // random unvisited neighbour
+                    var npos = randomNeighbour.Position;
+                    maze[current.X, current.Y] &= ~randomNeighbour.SharedWall; // remove wall from current cell
+                    maze[npos.X, npos.Y] &= ~GetOppositeWall(randomNeighbour.SharedWall); // remove wall from neighbour
+                    maze[npos.X, npos.Y] |= WallState.VISITED; // mark neighbour as visited
+                    posStack.Push(npos); // put neighbour onto stack as new position
+                }
+            }
+
+            return maze;
+        }
+
+        private WallState GetOppositeWall(WallState wall)
+        {
+            switch (wall)
+            {
+                case WallState.LEFT: return WallState.RIGHT;
+                case WallState.RIGHT: return WallState.LEFT;
+                case WallState.UP: return WallState.DOWN;
+                case WallState.DOWN: return WallState.UP;
+                default: return WallState.LEFT;
+            }
+        }
+
+        private List<Neighbour> GetUnvisitedNeighbours(Position p, WallState[,] maze, int width, int height)
+        {
+            var list = new List<Neighbour>();
+
+            if (p.X > 0) // LEFT
+                if (!maze[p.X - 1, p.Y].HasFlag(WallState.VISITED))
+                    list.Add(new Neighbour
+                    {
+                        Position = new Position { X = p.X - 1, Y = p.Y },
+                        SharedWall = WallState.LEFT
+                    });
+
+            if (p.Y > 0) // DOWN
+                if (!maze[p.X, p.Y - 1].HasFlag(WallState.VISITED))
+                    list.Add(new Neighbour
+                    {
+                        Position = new Position { X = p.X, Y = p.Y - 1 },
+                        SharedWall = WallState.DOWN
+                    });
+
+            if (p.Y < height - 1) // UP
+                if (!maze[p.X, p.Y + 1].HasFlag(WallState.VISITED))
+                    list.Add(new Neighbour
+                    {
+                        Position = new Position { X = p.X, Y = p.Y + 1 },
+                        SharedWall = WallState.UP
+                    });
+
+            if (p.X < width - 1) // RIGHT
+                if (!maze[p.X + 1, p.Y].HasFlag(WallState.VISITED))
+                    list.Add(new Neighbour
+                    {
+                        Position = new Position { X = p.X + 1, Y = p.Y },
+                        SharedWall = WallState.RIGHT
+                    });
+
+            return list;
+        }
+    }
+}
---
+++ MazeGenerator/MainWindow.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MazeGenerator.AppLogic;
+
+namespace MazeGenerator
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonGenerate_Click(object sender, RoutedEventArgs e)
+        {
+            int cols = 20, rows = 20;
+            var maze = MazeGen2.Generate(cols, rows);
+            DrawMaze(maze, 16); // a node in the maze is 16x16 pixels
+            
+        }
+
+        private void DrawMaze(WallState[,] maze,int scale)
+        {
+            myCanvas.Children.Clear();
+            for (int y = 0; y < 20; y++)
+                for (int x = 0; x < 20; x++)
+                {
+                    if (maze[x, y].HasFlag(WallState.UP)) DrawWall(x, y, WallState.UP, scale);
+                    if (maze[x, y].HasFlag(WallState.LEFT)) DrawWall(x, y, WallState.LEFT, scale);
+                    // optimized to only draw on last col and first row (canvas 0,0 is bottom left)
+                    if (y == 0 && maze[x, y].HasFlag(WallState.DOWN)) DrawWall(x, y, WallState.DOWN, scale);
+                    if (x == 19 && maze[x, y].HasFlag(WallState.RIGHT)) DrawWall(x, y, WallState.RIGHT, scale);
+                }
+        }
+
+        private void DrawWall(int x, int y, WallState wallState, int scale)
+        {
+            x = x * scale + 20; y = y * scale + 20;
+
+            var wall = new Line();
+            wall.Stroke = Brushes.Black;
+            wall.StrokeThickness = 2;
+            
+            // Note: maze is flipped horizontally because WPF Canvas 0,0 is bottom left
+            int s = scale / 2;
+            switch (wallState)
+            {
+                case WallState.DOWN:
+                    wall.X1 = x - s;
+                    wall.X2 = x + s;
+                    wall.Y1 = y - s;
+                    wall.Y2 = y - s;
+                    break;
+                case WallState.UP:
+                    wall.X1 = x - s;
+                    wall.X2 = x + s;
+                    wall.Y1 = y + s;
+                    wall.Y2 = y + s;
+                    break;
+                case WallState.LEFT:
+                    wall.X1 = x - s;
+                    wall.X2 = x - s;
+                    wall.Y1 = y - s;
+                    wall.Y2 = y + s;
+                    break;
+                case WallState.RIGHT:
+                    wall.X1 = x + s;
+                    wall.X2 = x + s;
+                    wall.Y1 = y - s;
+                    wall.Y2 = y + s;
+                    break;
+            }
+
+            
+            myCanvas.Children.Add(wall);
+        }
+    }
+}
